94f5c95f88a28d7719db370744e727eba36ec7ca
davidav <davidenkav@gmail.com>
2021-03-17 21:29:11 +0500

    1703

--- #src/js/swiperSettings.js
+++ #src/js/swiperSettings.js
@@ -1,8 +1,33 @@
 new Swiper('.image-slider', {
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+        dynamicBullets: true,
+    },
+    grabCursor: true,
+    mousewheel:{
+        sensitivity: 1,
+        eventsTarget:".image-slider"
+    },
+    // autoHeight: true,
+    loop: true,
+    autoplay:{
+        delay: 3000,
 
+    },
+    // breakpoints:{
+    //     320:{
+    //         slidesPerView: 1,
+    //     },
+    //     480:{
+    //         slidesPerView: 2,
+    //     },
+    //     992:{
+    //         slidesPerView: 3,
+    //     }
+    // },
 });
